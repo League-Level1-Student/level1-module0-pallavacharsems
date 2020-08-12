49733495114bdee73486ef4a394025f772da8263
Pallav <Pallav@DESKTOP-DVPQU53>
2020-08-12 15:01:32 -0700

    ALMOST DONE WITH JACK IN THE BOX!

--- src/_08_jack_in_the_box/JackInTheBox.java
+++ src/_08_jack_in_the_box/JackInTheBox.java
@@ -1,47 +1,79 @@
 package _08_jack_in_the_box;
 
+import java.applet.AudioClip;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.print.DocFlavor.URL;
 import javax.swing.Icon;
 import javax.swing.ImageIcon;
+import javax.swing.JApplet;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class JackInTheBox {
-public void showButton () {
-	JFrame frame = new JFrame();
-	
+public class JackInTheBox implements ActionListener {
+	public void showButton() {
+		int c = 1;
+		JButton button = new JButton("Surprise");
+		button.setVisible(true);
+		JFrame ice = new JFrame();
+		ice.setVisible(true);
+		ice.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel ic = new JPanel();
+		JLabel x = new JLabel();
+		ic.add(button);
+		ice.add(ic);
+		ic.add(x);
+		ice.pack();
+		button.addActionListener(this);
+
+	}
+
+	private void showPicture(String fileName) {
+		try {
+			JLabel imageLabel = createLabelImage(fileName);
+			JFrame frame = new JFrame();
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.add(imageLabel);
+			frame.setVisible(true);
+			frame.pack();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private JLabel createLabelImage(String fileName) {
+		try {
+			java.net.URL imageURL = getClass().getResource(fileName);
+			if (imageURL == null) {
+				System.err.println("Could not find image " + fileName);
+				return new JLabel();
+			} else {
+				Icon icon = new ImageIcon(imageURL);
+				JLabel imageLabel = new JLabel(icon);
+				return imageLabel;
+			}
+		} catch (Exception e) {
+			System.err.println("Could not find image " + fileName);
+			return new JLabel();
+		}
+	}
+
+	private void playSound(String soundFile) {
+		try {
+			AudioClip sound = JApplet.newAudioClip(getClass().getResource(soundFile));
+			sound.play();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		showPicture("jackInTheBox.png");
+	}
+
 }
-private void showPicture(String fileName) { 
-    try {
-         JLabel imageLabel = createLabelImage(fileName);
-         JFrame frame = new JFrame();
-         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-         frame.add(imageLabel);
-         frame.setVisible(true);
-         frame.pack();
-    } catch (Exception e) {
-         e.printStackTrace();
-    }
-}
-
-
-
-private JLabel createLabelImage(String fileName) {
-   try {
-	          java.net.URL imageURL = getClass().getResource(fileName);
-        if (imageURL == null) {
-             System.err.println("Could not find image " + fileName);
-             return new JLabel();
-         } else {
-              Icon icon = new ImageIcon(imageURL);
-              JLabel imageLabel = new JLabel(icon);
-              return imageLabel;
-        }
-    } catch (Exception e) {
-         System.err.println("Could not find image " + fileName);
-         return new JLabel();
-   }
-}
-
-}
-
